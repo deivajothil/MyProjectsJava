9a5a1a3a7d5b32998896196fa24e1d185cb0cc17
deivajothil <deivajothil@gmail.com>
2022-03-13 04:16:20 +0100

    Updated my code

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator
+{
+public static void main(String args[])
+{
+Calculator calc = new Calculator();
+calc.add();
+	calc.subtract();
+}
+	public void add()
+	{
+	System.out.println(10+5);
+	}
+	public void subtract()
+	{
+	System.out.println(10 - 5);
+	}
+}
